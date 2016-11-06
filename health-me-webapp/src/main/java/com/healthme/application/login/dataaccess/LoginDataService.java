e0aa6cf879e21a7348017862bedd601288b1836c
SHAIK,ASHRAF,ALI <ashraf.shaik@pearson.com>
2016-11-06 18:00:55 -0500

    Refactoring

--- health-me-webapp/src/main/java/com/healthme/application/login/dataaccess/LoginDataService.java
+++
@@ -1,7 +0,0 @@
-package com.healthme.application.login.dataaccess;
-
-import java.util.Properties;
-
-public class LoginDataService {
-    protected Properties queries;
-}
--- health-me-webapp/src/main/java/com/healthme/application/login/service/ILoginServcie.java
+++
@@ -1,8 +0,0 @@
-package com.healthme.application.login.service;
-
-import com.healthme.application.login.entity.Patient;
-
-public interface ILoginServcie {
-
-    boolean isUserValidUser(Patient patient);
-}
